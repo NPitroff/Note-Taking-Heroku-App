08aa65dd74ed39f2443421f31b7db55ae4957970
NPitroff <40879599+NPitroff@users.noreply.github.com>
2020-11-30 10:26:33 -0800

    Added an 'app.listen' command to the js file

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -11,8 +11,15 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 require("../html/index.html")(app);
 require("../html/notes.html")(app);
+
+// The below code effectively "starts" our server
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
 
 const $noteTitle = $(".note-title");
 const $noteText = $(".note-textarea");
